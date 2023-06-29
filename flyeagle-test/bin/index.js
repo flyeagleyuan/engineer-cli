2f6402b16c78bdffd555c32c936337c4f444826a
feixiao.yuan <feixiao.yuan@linkedsmile.com>
2023-06-29 20:58:46 +0800

    feat: change something

--- flyeagle-test/bin/index.js
+++ flyeagle-test/bin/index.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs/yargs')
+const lib = require('flyeagle-test-lib')
+const argv = require('process').argv
+
+const commond = argv[2]
+
+if(lib[commond]){
+  lib[commond]()
+}else{
+  console.log('请输入命令')
+}
+
+
+console.log(lib.sum(1,3))
 
 yargs()
