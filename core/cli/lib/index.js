c186edb6668200a47de41a1b144afddfa4817467
feixiao.yuan <feixiao.yuan@linkedsmile.com>
2023-08-08 14:13:54 +0800

    feat: change pack dir

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
+
 module.exports = core;
 
 function core() {
+    console.log('exec core')
+
     return "Hello from core";
 }
--- utils/utils/lib/index.js
+++ utils/utils/lib/index.js
@@ -3,5 +3,6 @@
 module.exports = utils;
 
 function utils() {
+    console.log("engineer-cli-dev!! utils");
     return "Hello from utils";
 }
